ab8111972567c6389fb74a65acd27ed27953463d
tarekahsan <tarekahsan709@gmail.com>
2021-07-26 15:50:04 +0600

    Add browser structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+// /* eslint-disable */
 const program = require('commander');
+const open = require('open');
 
 const cli = require('./cli');
 const { PLATFORM } = require('./constant');
@@ -7,12 +9,25 @@ const { PLATFORM } = require('./constant');
 program
   .version('1.0.0', '-v, --version', 'output the current version')
   .description('The rock, paper, scissor game')
-  .option('-p, --play', 'start the game');
+  .option('-p, --play', 'start the game')
+  .option('-b, --browser', 'open the game in browser');
 
 program.parse(process.argv);
 
 async function openBrowser() {
-  // Open browser here
+  await open(`${__dirname}/web/index.html`);
+
+  /* FIXME: Need to check using server
+  const server = http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(`${__dirname}/web/index.html`)
+      .pipe(res);
+  });
+  server.listen(3000, ()=>{
+    console.log('Server is running');
+  });
+  await open(`localhost:3000`);
+  */
 }
 
 async function startGame() {
@@ -27,4 +42,6 @@ async function startGame() {
 const options = program.opts();
 if (options.play) {
   startGame();
+} else if (options.browser) {
+  openBrowser();
 }
